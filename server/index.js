1e8f44872e5e90149bf830436e21a533dd3e168c
jaimemendozadev <jaimemendozadev@gmail.com>
2017-04-29 12:23:07 -0700

    Add join table to db schema

--- server/index.js
+++ server/index.js
@@ -14,5 +14,4 @@ app.use(require('./routers/router.styles'));
 app.use(require('./routers/router.transactions'));
 
 app.get('/', (req, res) => res.send('hello world'));
-
 app.listen(process.env.PORT, () => console.log(`Listening on http://localhost:${process.env.PORT}`));
