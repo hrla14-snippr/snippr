9cef71a72b74e8ff488d6a832c1c8e25f26c94f7
Keebin <kirikliu@gmail.com>
2017-04-29 10:52:37 -0700

    Set up server routes

---
+++ server/controllers/controller.login.js
@@ -0,0 +1,7 @@
+exports.login = (req, res) => {
+  res.send('login');
+};
+
+exports.signup = (req, res) => {
+  res.send('signup');
+};
---
+++ server/routers/router.barberLocations.js
@@ -0,0 +1,6 @@
+const Router  = require('express').Router();
+const barberLocationController = require('../controllers/controller.barberlocations');
+
+Router.get('/nearbyBarbers', barberLocationController.fetchBarbers);
+
+module.exports = Router;
