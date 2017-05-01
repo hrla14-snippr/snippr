5bbe4e1b11896c3d24d6115d681a4813fbb941aa
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-04-30 21:37:37 -0700

    add Dashboard for Clients
    Dashboard is connected to redux's store and uses it's nearbybarbers property to update it's own state. Also import fetchbarber action which is dispatched to redux store so we can use it as a prop to grab all the nearby barbers
    
    - lists out all barbers first name and last name

---
+++ dev/containers/ClientDashboard.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FetchBarbers } from '../actions/FetchBarbers';
+
+class ClientDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nearbyBarbers: this.props.nearbyBarbers,
+    };
+  }
+  componentDidMount() {
+    console.log(this.props, 'my props ho');
+    this.props.FetchBarbers();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Client Dashboard</h1>
+        <h2>Nearby Barbers</h2>
+        <ul>
+          {this.props.nearbyBarbers.map(barber => (
+            <div>
+              <p>{barber.fname}</p>
+              <p>{barber.lname}</p>
+            </div>
+            ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  nearbyBarbers: state.nearbyBarbers,
+});
+
+ClientDashboard.propTypes = {
+  nearbyBarbers: PropTypes.Array,
+  FetchBarbers: PropTypes.Function,
+};
+
+export default connect(mapStateToProps, { FetchBarbers })(ClientDashboard);
+
