d8f5fc8a0a4f1fbf134b2556ea4a70a8d8d16127
kevinliu6102 <kirikliu@gmail.com>
2017-04-30 22:31:51 -0700

    Update FetchBarbers.js

--- dev/actions/FetchBarbers.js
+++ dev/actions/FetchBarbers.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const URL = 'http://localhost:3000/nearbyBarbers';
 
 exports.FetchBarbers = () => {
-  const request = axios.get(`${URL}`);
+  const request = axios.get(URL);
   console.log('fetch barbers ran');
   return ({
     type: 'FETCH_BARBERS',
