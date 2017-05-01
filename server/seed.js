5a578a13d502c86891455068e7e0469b0f73ebf2
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-04-30 21:15:53 -0700

    made a seed.js file for us to test app with us
    imported database models to use first. Imported dummy data from data.js file after. Wrote function that destroys all the models in the table asynchronously connected through .then's. After I wrote function that uses bulk create to fill database with dummy data.

---
+++ server/seed.js
@@ -0,0 +1,34 @@
+const db = require('./models/db');
+const Barbers = require('./data');
+
+// delete info in tables
+db.Barber.destroy({ where: {} })
+  .then(() => {
+    db.Client.destroy({ where: {} });
+  })
+  .then(() => {
+    db.Style.destroy({ where: {} });
+  })
+  .then(() => {
+    db.Transaction.destroy({ where: {} });
+  })
+  .then(() => {
+    db.BarberStyles.destroy({ where: {} });
+  })
+  .catch((err) => {
+    console.log('error occured destroying models ', err);
+  });
+
+console.log('these are the barbers', Barbers);
+// create barbers (testing)
+db.Barber.bulkCreate(Barbers.Barber)
+  .then(() => { // Notice: There are no arguments here, as of right now you'll have to...
+    return db.Barber.findAll();
+  })
+  .then((barbers) => {
+    console.log('created list of barbers', barbers); // ... in order to get the array of user objects
+  })
+  .catch((err) => {
+    console.log('error occured trying to bulk create barbers ', err);
+  });
+
