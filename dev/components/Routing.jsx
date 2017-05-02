473903623f506d76803ea774048ff95698289d20
Keebin <kirikliu@gmail.com>
2017-05-01 17:32:50 -0700

    Scaffold gating logic for receiving profile information from new users

--- dev/components/Routing.jsx
+++ dev/components/Routing.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 import AuthService from '../utils/AuthService';
 import LandingPage from './LandingPage';
+import UserInfoForm from './UserInfoForm';
 import Login from './Login';
 import ClientDashboard from '../containers/ClientDashboard';
 
@@ -11,13 +13,36 @@ class Routing extends Component {
     super(props);
     this.state = {
       auth: new AuthService('enONSvCznucqb91b3s0guCDKxX5Ce6KO', 'kirisakima.auth0.com', props.history),
+      hasProfile: false,
     };
     this.renderHomepage = this.renderHomepage.bind(this);
+    this.submitUserInfo = this.submitUserInfo.bind(this);
+  }
+
+  submitUserInfo(e) {
+    e.preventDefault();
+    const data = {};
+    Array.prototype.slice.call(e.target.children).forEach((childNode, idx, arr) => {
+      if (idx < 3) {
+        data[childNode.name] = childNode.value;
+      } else if (idx < arr.length - 1) {
+        data[childNode.children[0].name] = childNode.children[0].checked;
+      }
+    });
+    console.log(data);
+    // setstate in axios callback
+    this.setState({ hasProfile: true });
+    this.props.history.push('/');
+  }
+
+  renderUserInfoForm() {
+    return this.state.hasProfile
+      ? <LandingPage logout={this.state.auth.logout} />
+      : <Redirect to="/newUser" />;
   }
 
   renderHomepage() {
-    console.log('logged in?', this.state.auth.loggedIn());
-    return this.state.auth.loggedIn() ? <LandingPage logout={this.state.auth.logout} /> : <Redirect to={'/login'} />;
+    return this.state.auth.loggedIn() ? this.renderUserInfoForm() : <Redirect to="/login" />;
   }
 
   render() {
@@ -31,6 +56,9 @@ class Routing extends Component {
         </Route>
         <Route exact path="/cdashboard">
           <ClientDashboard />
+        </Route>
+        <Route exact path="/newUser">
+          <UserInfoForm submitUserInfo={this.submitUserInfo} />
         </Route>
       </Switch>
     );
