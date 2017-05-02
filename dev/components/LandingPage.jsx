9e4eb1212158a79300c1b3802654e5702e9e9ed0
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-01 23:23:13 -0700

    update landing page
    -add background image
    -add signup buttons
    -add snyppr text top left
    -add login-button that links to login page on right

--- dev/components/LandingPage.jsx
+++ dev/components/LandingPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const LandingPage = () =>
   (
     <div className="landing-page">
-      landing page
+      <button className="snypee-but">Click here to be a snypee</button>
+      <button className="snyppr-but">Click here to be a snyppr</button>
+      <Link to="/login"><button className="login">Login</button></Link>
+      <div className="logo">Snyppr</div>
+      <div className="left-overlay" >
+        <div className="left-text">Are you a Snypee?</div>
+      </div>
+      <div className="right-overlay" >
+        <div className="right-text">Are you a Snyppr?</div>
+      </div>
     </div>
   );
 
