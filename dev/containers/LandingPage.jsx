7ccbda50b14041bc251a44cecde5678875be1bcb
Keebin <kirikliu@gmail.com>
2017-05-03 09:52:09 -0700

    Hook up signup buttons with partially working dynamic AccountType prefill

--- dev/containers/LandingPage.jsx
+++ dev/containers/LandingPage.jsx
@@ -8,25 +8,36 @@ import { SetAccountType } from '../actions/SetAccountType';
 class LandingPage extends Component {
   constructor(props) {
     super(props);
-    console.log(props.accountType);
+
     this.state = {
       accountType: props.accountType,
       auth: new AuthService('enONSvCznucqb91b3s0guCDKxX5Ce6KO', 'kirisakima.auth0.com', props.history, props.accountType),
     };
+
+    this.typedSignup = this.typedSignup.bind(this);
+  }
+
+  typedSignup(e) {
+    // TODO: Fix to work every time
+    this.props.SetAccountType(e);
+    this.setState({
+      auth: new AuthService('enONSvCznucqb91b3s0guCDKxX5Ce6KO', 'kirisakima.auth0.com', this.props.history, this.props.accountType),
+    });
+
+    this.state.auth.signup();
   }
 
   render() {
     const { history } = this.props; // eslint-disable-line no-unused-vars
-    console.log(this.props.accountType);
 
     const SNYPEE = 'snypee';
     const SNYPPR = 'snyppr';
     return (
       <div className="landing-page">
-        <button onClick={this.props.SetAccountType} value={SNYPEE} className="snypee-but">
+        <button onClick={this.typedSignup} value={SNYPEE} className="snypee-but">
           Click here to be a snypee
         </button>
-        <button onClick={this.props.SetAccountType} value={SNYPPR} className="snyppr-but">
+        <button onClick={this.typedSignup} value={SNYPPR} className="snyppr-but">
           Click here to be a snyppr
         </button>
         <button onClick={this.state.auth.login} className="login">Login</button>
