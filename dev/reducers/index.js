45c9b8b08a24fa8e8d505aa9b64f85f3fc96a17f
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-04 08:39:39 -0700

    delete fetchbarber reducer

--- dev/reducers/index.js
+++ dev/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import NearbySnypprReducer from './nearbySnyppr_reducer';
 import AccountTypeReducer from './accounttype_reducer';
 
 const rootReducer = combineReducers({
