2120cdab204a6414a8e01db64c5304f593768603
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-03 19:51:10 -0700

    Create working version of Barber Dashboard

--- dev/containers/BarberDashboard.jsx
+++ dev/containers/BarberDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Menu, Image, Segment, List } from 'semantic-ui-react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { Image, List } from 'semantic-ui-react';
 
 class BarberDashboard extends Component {
   constructor() {
@@ -9,45 +10,36 @@ class BarberDashboard extends Component {
   render() {
     return (
       <div>
-        
-        <Menu size="big" className="snyppr-menu" inverted >
-          <Menu.Item name="home" />
-          <Menu.Item name="messages" />
-          <Menu.Menu position="right">
-            <Menu.Item name="logout" onClick={this.props.logout} />
-          </Menu.Menu>
-        </Menu>
-
-        BarberDashboard
-
-
-        {/*<Image src="https://d1w2poirtb3as9.cloudfront.net/4d3bab3df8c05d96ddf9.jpeg" size="medium" shape="circular" />
-         Hi Ebrima*/}
-
-        <List className="snyppr-list" selection verticalAlign="middle">
-          <List.Item>
-            <List.Content>
-              <List.Header>Home</List.Header>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>Profile</List.Header>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>Payment</List.Header>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content onClick={this.props.logout}>
-              <List.Header>Log Out</List.Header>
-            </List.Content>
-          </List.Item>
-        </List>
-       
-
+        <nav className="snyppr-menu" />
+        <Grid>
+          <Row className="show-grid">
+            <Col xs={6} md={4}><code>
+              <Image className="snyppr-img" src="https://d1w2poirtb3as9.cloudfront.net/4d3bab3df8c05d96ddf9.jpeg" size="small" shape="circular" />
+              <List className="snyppr-list" selection verticalAlign="middle">
+                <List.Item>
+                  <List.Content>
+                    <List.Header>Home</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header>Profile</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header>Payment</List.Header>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Header>Log Out</List.Header>
+                  </List.Content>
+                </List.Item>
+              </List>
+            </code></Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
