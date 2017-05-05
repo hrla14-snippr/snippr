168a9a93df5b34fa7227cc890af6f390a272d819
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-05 16:41:35 -0700

    add favoritelist component to hold favorite snypprs

---
+++ dev/components/FavoriteEntry.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ChangeSnyppr } from '../actions/ChangeSnyppr';
+
+const FavoriteEntry = (props) => {
+  console.log(props, 'this is snypprentry');
+  return (
+    <Link to="/snypprProfile">
+      <div onClick={() => props.ChangeSnyppr(props.favorite)} className="snypprentry">
+        <div className="entryimg">
+          <img alt="placeholder" src="https://timeforgeography.co.uk/static/img/avatar-placeholder.png" height="50px" width="50px" />
+        </div>
+        <div>
+          <h1 className="entryheader">{props.favorite.fname} {props.favorite.lname}</h1>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+FavoriteEntry.propTypes = {
+  favorite: PropTypes.shape.isRequired,
+  ChangeSnyppr: PropTypes.func.isRequired,
+};
+
+
+export default connect(null, { ChangeSnyppr })(FavoriteEntry);
