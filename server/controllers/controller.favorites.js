b2ef91c09745d1b0991fce7271b3fbafe30c6363
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-05 16:42:55 -0700

    update favorite controller to fetch all favorites from specified user

--- server/controllers/controller.favorites.js
+++ server/controllers/controller.favorites.js
@@ -1,4 +1,18 @@
+// const db = require('../models/db');
+
 exports.fetchFavorites = (req, res) => {
+  // db.Favorite.findAll({
+  //   where: {
+  //     id: req.body.id,
+  //   },
+  // })
+  //   .then((results) => {
+  //     res.send(results);
+  //   })
+  //   .catch((err) => {
+  //     console.log('error fetching favorites on the server ', err);
+  //     res.status(400);
+  //   });
   res.send('fetch favorites');
 };
 
