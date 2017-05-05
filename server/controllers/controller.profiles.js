83284b418576dbf800d102a9a65936294a030495
Keebin <kirikliu@gmail.com>
2017-05-04 17:40:35 -0700

    Remove comments from profiles controller

--- server/controllers/controller.profiles.js
+++ server/controllers/controller.profiles.js
@@ -2,9 +2,6 @@ const db = require('../models/db');
 // store whether user is snypee or snyppr in auth0 profile
 
 exports.verifyHasProfile = (req, res) => {
-  // check if any row in barber/clients table has this id
-  // req.body.authId
-  console.log(req.body);
   const accountType = req.body.accountType;
   delete req.body.accountType;
   db[accountType]
@@ -16,14 +13,6 @@ exports.verifyHasProfile = (req, res) => {
 };
 
 exports.addProfile = (req, res) => {
-  // req.body will contain profile info
-  // id: "authId string format"
-  // address:"asdf"
-  // fname: "asdf"
-  // lname: "asdf"
-  // styles: '1246' concatted ids of styles for this user
-  // save this to DB
-  console.log('profile', req.body);
   const accountType = req.body.accountType;
   delete req.body.accountType;
   db[accountType]
