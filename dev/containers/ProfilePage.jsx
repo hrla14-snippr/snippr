22ac47513466cd44039c9d068f2ae46bf18eb920
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-05 13:41:37 -0700

    Create profile page from dashboard styling

--- dev/containers/ProfilePage.jsx
+++ dev/containers/ProfilePage.jsx
@@ -4,15 +4,33 @@ import PropTypes from 'prop-types';
 import ClientChat from './ClientChat';
 
 const ProfilePage = props => (
-  <div>
-    <h1>{props.snyppr.fname} {props.snyppr.lname}</h1>
-    <p>{props.snyppr.address}</p>
-    {/* Place Stripe Button Here*/}
-    <button>Stripe</button>
-    <ClientChat name={`${props.snyppr.fname}${props.snyppr.lname}`} />
+  <div className="profile">
+
+    <div className="clientheader">
+      <h1 className="clientheadline">Snyppr</h1>
+    </div>
+
+    {/* Main Body*/}
+    <div className="profile-box">
+
+
+      <h1>{props.snyppr.fname} {props.snyppr.lname}</h1>
+      <p>{props.snyppr.address}</p>
+      <button>Stripe</button>
+      <ClientChat name={`${props.snyppr.fname}${props.snyppr.lname}`} />
+
+    </div>
+
+
+    <div className="clientfooter" >
+      <span className="footerdet">Refer Friends</span>
+      <span className="footerdet">About Us</span>
+      <span className="footerdet">Become Snyppr</span>
+    </div>
+
   </div>
-  )
-;
+
+  );
 
 ProfilePage.propTypes = {
   snyppr: PropTypes.shape.isRequired,
