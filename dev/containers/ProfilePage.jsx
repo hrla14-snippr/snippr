18597a91a1574d1d630c2f136779ca17d10c3171
Keebin <kirikliu@gmail.com>
2017-05-04 17:37:03 -0700

    Update proptype checking and connect to redux store

--- dev/containers/ProfilePage.jsx
+++ dev/containers/ProfilePage.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const ProfilePage = props => (
   <div>
-    <h1>{props.fname} {props.lname}</h1>
-    <p>{props.address}</p>
+    <h1>{props.snyppr.fname} {props.snyppr.lname}</h1>
+    <p>{props.snyppr.address}</p>
     {/* Place Stripe Button Here*/}
   </div>
   )
 ;
 
 ProfilePage.propTypes = {
-  fname: PropTypes.string.isRequired,
-  lname: PropTypes.string.isRequired,
-  address: PropTypes.string.isRequired, // eslint-disable-line react/forbid-prop-types
+  snyppr: PropTypes.shape.isRequired,
 };
 
-export default ProfilePage;
+const mapStateToProps = state => ({
+  snyppr: state.currentSnyppr,
+});
+
+export default connect(mapStateToProps)(ProfilePage);
 
