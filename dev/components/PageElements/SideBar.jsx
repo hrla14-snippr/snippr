32d3e215f75e4c197366e0eb36d96434a09e6625
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-05 19:38:46 -0700

    Start work on making favorite icon functional

--- dev/components/PageElements/SideBar.jsx
+++ dev/components/PageElements/SideBar.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
+const axios = require('axios');
+
 class SideBar extends Component {
   constructor(props) {
     super(props);
+    this.handleFav = this.handleFav.bind(this);
+  }
+
+  handleFav() {
+    const snypeeId = JSON.parse(window.localStorage.profile).user_id;
+    axios.post('/favorites', {
+      snypprId: 'Fred',
+      snypeeId,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
@@ -19,7 +36,8 @@ class SideBar extends Component {
             <path
               id="heart-path" d="M16,28.261c0,0-14-7.926-14-17.046c0-9.356,
               13.159-10.399,14-0.454c1.011-9.938,14-8.903,14,0.454C30,20.335,
-              16,28.261,16,28.261z"/>
+              16,28.261,16,28.261z"
+            />
           </svg>
         </div>
 
