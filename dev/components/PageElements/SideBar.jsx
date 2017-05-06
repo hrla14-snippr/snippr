e802de1aec910919ae405a6df0dd86a41f405e49
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-05 18:30:25 -0700

    Add favorite svg to sidebar

--- dev/components/PageElements/SideBar.jsx
+++ dev/components/PageElements/SideBar.jsx
@@ -13,6 +13,17 @@ class SideBar extends Component {
       <div className="sidebarmenu">
         <div className="sidebar-items">Work</div>
         <div className="sidebar-items">Reviews</div>
+
+        <div className="sidebar-items svgcontainer">
+          <svg viewBox="0 0 32 32" id="heart">
+            <path
+              id="heart-path" d="M16,28.261c0,0-14-7.926-14-17.046c0-9.356,
+              13.159-10.399,14-0.454c1.011-9.938,14-8.903,14,0.454C30,20.335,
+              16,28.261,16,28.261z"/>
+          </svg>
+        </div>
+
+
       </div>
     </div>
     );
