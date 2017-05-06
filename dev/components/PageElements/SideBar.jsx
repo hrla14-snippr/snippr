f9b703e77bd26b76f27af8eed7879ac21f11ba43
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-06 06:50:18 -0700

    Add onClick functionality to favorite svg
    Made favorite svg into separate component to enable onClick

--- dev/components/PageElements/SideBar.jsx
+++ dev/components/PageElements/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import FavoriteIcon from './FavoriteIcon';
 
 const axios = require('axios');
 
@@ -10,6 +11,8 @@ class SideBar extends Component {
   }
 
   handleFav() {
+    console.log('inside handleFav click');
+
     const snypeeId = JSON.parse(window.localStorage.profile).user_id;
     axios.post('/favorites', {
       snypprId: this.props.snypprId,
@@ -31,32 +34,18 @@ class SideBar extends Component {
       <div className="sidebarmenu">
         <div className="sidebar-items">Work</div>
         <div className="sidebar-items">Reviews</div>
-
-        <div className="sidebar-items svgcontainer">
-          <svg viewBox="0 0 32 32" id="heart">
-            <path
-              id="heart-path" d="M16,28.261c0,0-14-7.926-14-17.046c0-9.356,
-              13.159-10.399,14-0.454c1.011-9.938,14-8.903,14,0.454C30,20.335,
-              16,28.261,16,28.261z"
-            />
-          </svg>
+        <div onClick={this.handleFav} className="sidebar-items svgcontainer">
+          <FavoriteIcon />
         </div>
-
-
       </div>
     </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    snypprId: state.currentSnyppr,
-  }
-}
+const mapStateToProps = state => ({
+  snypprId: state.currentSnyppr,
+});
 
 
 export default connect(mapStateToProps)(SideBar);
-
-
-
