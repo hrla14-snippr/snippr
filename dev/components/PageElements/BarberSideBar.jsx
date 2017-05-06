0d0403a8ebb96d4450a20e98139f5bee52e27fbf
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-06 13:11:02 -0700

    Fix BarberDashboard with new css styling and layout

--- dev/components/PageElements/BarberSideBar.jsx
+++ dev/components/PageElements/BarberSideBar.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-
-class SideBar extends Component {
+class BarberSideBar extends Component {
   constructor(props) {
     super(props);
   }
@@ -15,17 +14,17 @@ class SideBar extends Component {
       <div className="sidebarmenu">
         <div className="sidebar-items">Work</div>
         <div className="sidebar-items">Reviews</div>
-        <div onClick={this.props.logout} className="navmenu-items">Logout</div>
+        <div className="sidebar-items"><a href="https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_AZdNGhV2VUe0pilvxfh5jkNfsevELTNz&scope=read_write&redirect_uri=http://localhost:3000/stripeId">Payment Setup</a></div>
+        <div onClick={this.props.logout} className="sidebar-items">Logout</div>
       </div>
     </div>
     );
   }
 }
 
-SideBar.propTypes = {
+BarberSideBar.propTypes = {
   logout: PropTypes.func.isRequired,
 };
 
 
-export default SideBar;
-
+export default BarberSideBar;
