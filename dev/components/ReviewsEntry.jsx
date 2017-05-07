63cfd693f05defb31fc7bcdcdb79253fb25b366a
Keebin <kirikliu@gmail.com>
2017-05-07 12:49:54 -0700

    Save review components

---
+++ dev/components/ReviewsEntry.jsx
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react';
+
+// class ReviewsEntry extends Component {
+//   render(props) {
+//     return (
+//       <div>
+
+//       </div>
+//     );
+//   }
+// }
+
+// export default ReviewsEntry;
---
+++ dev/components/ReviewsList.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const ReviewsList = (props) => {
+//   return (
+//     <div>
+//       {props.reviews.map(review => <ReviewEntry review={review}/>)}
+//     </div>
+//   );
+// };
+
+// export default ReviewsList;
