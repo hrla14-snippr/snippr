436720f7fb14ed4fb92d7bb40c74d7936027be1b
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-08 10:21:08 -0700

    Create StripeSignUp component

--- dev/components/StripeSignUp.jsx
+++ dev/components/StripeSignUp.jsx
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types';
 
 const StripeSignUp = props => (
   <div className="stripesignup">
+<<<<<<< HEAD
     <div className="stripesignheader"><h1>Sign up for a Stripe Account</h1></div>
     <div className="stripesigncontent">
       <p>You will need a Stripe Account to continue using our service.</p>
       <a href={props.stripeURL}>Click on the link to sign up for an account!</a>
     </div>
+=======
+    <div className="formheader">Sign up for a Stripe Account</div>
+    <p>You will need a Stripe Account to continue using our service.
+          <a href={props.stripeURL}> Click on the link</a> to sign up for an account!</p>
+>>>>>>> Create StripeSignUp component
   </div>);
 
 StripeSignUp.propTypes = {
