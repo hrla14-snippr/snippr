aecd7222f06201e8c91aeb0a1ce658535bc58af0
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-08 11:25:01 -0700

    Create polish version of Stripe Signup Page
    Need to create a button version of the link

--- dev/components/StripeSignUp.jsx
+++ dev/components/StripeSignUp.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 
 const StripeSignUp = props => (
   <div className="stripesignup">
-    <div className="formheader">Sign up for a Stripe Account</div>
-    <p>You will need a Stripe Account to continue using our service.
-          <a href={props.stripeURL}> Click on the link</a> to sign up for an account!</p>
+    <div className="stripesignheader"><h1>Sign up for a Stripe Account</h1></div>
+    <div className="stripesigncontent">
+      <p>You will need a Stripe Account to continue using our service.</p>
+      <a href={props.stripeURL}>Click on the link to sign up for an account!</a>
+    </div>
   </div>);
 
 StripeSignUp.propTypes = {
