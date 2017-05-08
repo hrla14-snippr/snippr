e0a62874e6be4448bc6f1ef44e778a007770fa92
Mariano Okpalefe <babytron@Marianos-MacBook-Pro.local>
2017-05-08 00:51:39 -0700

    Update barber dashboard click logic

--- dev/components/PageElements/BarberSideBar.jsx
+++ dev/components/PageElements/BarberSideBar.jsx
@@ -4,16 +4,26 @@ import PropTypes from 'prop-types';
 class BarberSideBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
-
   render() {
     return (<div className="sidebar">
       <div className="picturebox">
         <img className="userpic" alt="placeholderimage" src="https://timeforgeography.co.uk/static/img/avatar-placeholder.png" />
       </div>
       <div className="sidebarmenu">
-        <div className="sidebar-items">Work</div>
-        <div className="sidebar-items">Reviews</div>
+        <button
+          onClick={this.props.changeWindow}
+          value="Work" className="navmenu-items"
+        >Work</button>
+        <button
+          onClick={this.props.changeWindow}
+          value="Reviews" className="navmenu-items"
+        >Reviews</button>
+        <button
+          className="navmenu-items"
+          onClick={this.props.changeWindow} value="Transactions"
+        >Transactions</button>
         <div onClick={this.props.logout} className="sidebar-items">Logout</div>
       </div>
     </div>
@@ -23,6 +33,7 @@ class BarberSideBar extends Component {
 
 BarberSideBar.propTypes = {
   logout: PropTypes.func.isRequired,
+  changeWindow: PropTypes.func.isRequired,
 };
 
 
