0c5c43fbd58c3e331981c5c240cbf9ba4500403c
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-08 12:11:19 -0700

    Make final styling changes to Stripe Account Signup
    Tried rendering Sign up link as a button. Chrome
    does not properly render a button. Renders a link
    instead.

--- dev/components/StripeSignUp.jsx
+++ dev/components/StripeSignUp.jsx
@@ -6,8 +6,9 @@ const StripeSignUp = props => (
     <div className="stripesignheader"><h1>Sign up for a Stripe Account</h1></div>
     <div className="stripesigncontent">
       <p>You will need a Stripe Account to continue using our service.</p>
-      <a className="stripeacctbtn" href={props.stripeURL}>
-        <button>Click on the link to sign up for an account!</button></a>
+      <button className="stripeacctbtn">
+        <a href={props.stripeURL}>Click on the link to sign up for an account!</a>
+      </button>
     </div>
     <div className="formheader">Sign up for a Stripe Account</div>
     <p>You will need a Stripe Account to continue using our service.
