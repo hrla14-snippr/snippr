f9ed110e0a8f4e648288bc472a9cf4a010e810ac
Shakeel Saleemi <shakahs@gmail.com>
2017-05-10 14:44:06 -0700

    Disable some rules from the AirBNB linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,10 @@ module.exports = {
         "no-underscore-dangle": [2, { "allowAfterThis": true, allow: ["_*"] }],
         "class-methods-use-this": 0,
         "no-static-element-interactions": 0,
-        "jsx-a11y/no-static-element-interactions": 0
+        "jsx-a11y/no-static-element-interactions": 0,
+        "import/first": 0,
+        "react/prefer-stateless-function": 0,
+        "max-len": [1, 120, 2, {ignoreComments: true}],
     },
     "globals": {
       "AUTH0_DOMAIN": false,
