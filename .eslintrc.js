80368be65738bfa1d3c4e84fa26bc4069971640f
Shakeel Saleemi <shakahs@gmail.com>
2017-05-09 19:32:45 -0700

    Move global variables from package.json to .eslintrc which overrides package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,9 @@ module.exports = {
         "no-underscore-dangle": [2, { "allowAfterThis": true, allow: ["_*"] }],
         "class-methods-use-this": 0,
         "no-static-element-interactions": 0,
+    },
+    "globals": {
+      "AUTH0_DOMAIN": false,
+      "AUTH0_CLIENT_ID": false
     }
 };
