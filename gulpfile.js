b5b0731e20a0817d07b3e37eebdf068051f144ad
Shakeel Saleemi <shakahs@gmail.com>
2017-05-09 19:55:41 -0700

    Reimplement schema creation and data seeding

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,39 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const env = require('gulp-env');
+const sequelizeFixtures = require('sequelize-fixtures');
 
 env({
   file: './.env',
   type: 'ini',
 });
+
+const db = require('./server/models/db');
+
+const models = {
+  Snyppr: db.Snyppr,
+};
+
+gulp.task('seed:wipe', (cb) => {
+  db.Snyppr.sync({ force: true })
+    .then(() => db.Snypee.sync({ force: true }))
+    .then(() => db.Transaction.sync({ force: true }))
+    .then(() => db.Favorite.sync({ force: true }))
+    .then(() => db.SnypprReview.sync({ force: true }))
+    .then(() => db.SnypeeReview.sync({ force: true }))
+    .then(() => { cb(); })
+    .catch((err) => { cb(err); });
+});
+
+gulp.task('seed:seed', ['seed:wipe'], (cb) => {
+  sequelizeFixtures.loadFile('./server/models/seedData/data.json', models)
+    .then(() => {
+      cb();
+    })
+    .catch((err) => { cb(err); });
+});
+
+gulp.task('seed', ['seed:wipe', 'seed:seed']);
 
 gulp.task('nodemon', () => {
   const stream = nodemon({
